3b497e9d90250349a125b9198bca5afbbaf49f2b
vipul1546 <vipul.t220@gmail.com>
2016-02-09 12:17:45 +0530

    name and random number

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php 
-	function name_rand ($name){
-		$rand=rand(6,15);
-		$out = $name . $rand;
-		return $out;
-	}
-echo name_rand("vipul");
+	function name_rand ($name){ // function start
+		$rand=rand(6,15); // Generate random number between 6 and 15
+		return $name . $rand;  // return output with name followed by random number
+	} // function stop
+
+echo name_rand("vipul"); // calling function for output
 ?>
